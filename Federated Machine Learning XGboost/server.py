5fcdfa3752ff69633538292cb07992da75edd49b
jackmcintyre333 <131042229+jackmcintyre333@users.noreply.github.com>
2024-10-21 16:09:00 -0400

    Fed Learning to version control

---
+++ Federated Machine Learning XGboost/server.py
@@ -0,0 +1,61 @@
+"""xgboost_quickstart: A Flower / XGBoost app."""
+
+from typing import Dict
+
+from flwr.common import Context, Parameters
+from flwr.server import ServerApp, ServerAppComponents, ServerConfig
+from flwr.server.strategy import FedXgbBagging
+import logging
+# Configure logging
+logging.basicConfig(
+    filename='logfile',          
+    level=logging.DEBUG,         
+    format='%(asctime)s - %(levelname)s - %(message)s',  
+    filemode='w'                  
+)
+def evaluate_metrics_aggregation(eval_metrics):
+    """Return aggregated metrics for evaluation."""
+    total_num = sum([num for num, _ in eval_metrics])
+    
+    metrics_aggregated = {}
+    for metric in ["accuracy", "precision", "recall", "F1", "AUC"]:
+        metrics_aggregated[metric] = sum([metrics[metric] * num for num, metrics in eval_metrics]) / total_num
+    
+    return metrics_aggregated
+
+
+def config_func(rnd: int) -> Dict[str, str]:
+    """Return a configuration with global epochs."""
+    config = {
+        "global_round": str(rnd),
+    }
+    return config
+
+
+def server_fn(context: Context):
+    # Read from config
+    num_rounds = context.run_config["num-server-rounds"]
+    fraction_fit = context.run_config["fraction-fit"]
+    fraction_evaluate = context.run_config["fraction-evaluate"]
+
+    # Init an empty Parameter
+    parameters = Parameters(tensor_type="", tensors=[])
+
+    # Define strategy
+    strategy = FedXgbBagging(
+        fraction_fit=fraction_fit,
+        fraction_evaluate=fraction_evaluate,
+        evaluate_metrics_aggregation_fn=evaluate_metrics_aggregation,
+        on_evaluate_config_fn=config_func,
+        on_fit_config_fn=config_func,
+        initial_parameters=parameters,
+    )
+    config = ServerConfig(num_rounds=num_rounds)
+
+    return ServerAppComponents(strategy=strategy, config=config)
+
+
+# Create ServerApp
+app = ServerApp(
+    server_fn=server_fn,
+)
